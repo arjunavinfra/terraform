f3d4e66ab5a83550239fe9d81b3a26ebaa006f75
Arjun <arjun@infracloud.io>
2022-11-30 13:38:38 +0530

    added modules

--- variables.tf
+++ variables.tf
@@ -1,11 +1,3 @@
-variable "vpc_cidr" {
-  default = "192.168.0.0/20"
-}
-
-variable "subnet_cidr" {
-  default =  "192.168.1.0/24"
-}
-
 variable "aws_region" {
   default = "ap-south-1"
 }
---
+++ vpc/variables.tf
@@ -0,0 +1,11 @@
+variable "vpc_cidr" {
+  default = "192.168.0.0/20"
+}
+
+variable "subnet_cidr" {
+  default =  "192.168.1.0/24"
+}
+
+variable "aws_region" {
+  default = "ap-south-1"
+}
