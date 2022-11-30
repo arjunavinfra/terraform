45a32eeda82a2333648c1c6ed2916d4814769d9a
Arjun <arjun@infracloud.io>
2022-11-30 18:52:10 +0530

    completed the vpc part

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_nat_gateway" "nat_gw" {
 resource "aws_subnet" "subnet_public" {
   vpc_id     = aws_vpc.kubex_vpc.id
   cidr_block = "10.0.1.0/24"
-
+  map_public_ip_on_launch = true
   tags = {
     Name = "kubex subnet pub"
   }
@@ -48,18 +48,18 @@ resource "aws_subnet" "subnet_public" {
 resource "aws_subnet" "subnet_private" {
   vpc_id     = aws_vpc.kubex_vpc.id
   cidr_block = "10.0.2.0/24"
-
+  map_public_ip_on_launch = false 
   tags = {
     Name = "kubex subnet pri"
   }
 }
 
 resource "aws_route_table" "route_table_public" {
-  vpc_id = aws_vpc.kubex.id
+  vpc_id = aws_vpc.kubex_vpc.id
 
   route {
     cidr_block = "10.0.1.0/24"
-    gateway_id = aws_internet_gateway.example.id
+    gateway_id = aws_internet_gateway.igw.id
   }
 
   tags = {
@@ -67,3 +67,33 @@ resource "aws_route_table" "route_table_public" {
   }
 }
 
+
+resource "aws_route_table" "route_table_private" {
+  vpc_id = aws_vpc.kubex_vpc.id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "kubex route table private"
+  }
+}
+
+
+
+resource "aws_route" "route_public" {
+  route_table_id            = aws_route_table.route_table_public.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id                = aws_internet_gateway.igw.id
+  depends_on                = [aws_route_table.route_table_public]
+
+}
+
+resource "aws_route" "route_private" {
+  route_table_id            = aws_route_table.route_table_private.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id            = aws_nat_gateway.nat_gw.id
+  depends_on                = [aws_route_table.route_table_private]
+}
