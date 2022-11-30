0d6f68926cc33053e1e96a94cb0efb9c7c31609e
Arjun <arjun@infracloud.io>
2022-11-30 18:12:00 +0530

    added route table code

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "kubex_vpc" {
   cidr_block = "10.0.0.0/16"
     tags = {
-    Name = "kubex"
+    Name = "kubex vpc"
   }
 }
 
@@ -9,7 +9,7 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.kubex_vpc.id
 
   tags = {
-    Name = "kubex"
+    Name = "kubex igw"
   }
 }
 
@@ -19,6 +19,51 @@ resource "aws_eip" "eip" {
     aws_internet_gateway.igw
   ]
     tags = {
-    Name = "kubex"
+    Name = "kubex elasticIP"
   }
 }
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.subnet_public.id
+
+  tags = {
+    Name = "kubex nat gw"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_subnet" "subnet_public" {
+  vpc_id     = aws_vpc.kubex_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "kubex subnet pub"
+  }
+}
+
+resource "aws_subnet" "subnet_private" {
+  vpc_id     = aws_vpc.kubex_vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "kubex subnet pri"
+  }
+}
+
+resource "aws_route_table" "route_table_public" {
+  vpc_id = aws_vpc.kubex.id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.example.id
+  }
+
+  tags = {
+    Name = "kubex route table pub"
+  }
+}
+
