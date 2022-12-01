a48a3d6101111904beb5251fb7b3ed9530db8b1c
Arjun <arjun@infracloud.io>
2022-12-01 18:53:02 +0530

    asdad

--- modules/RDS/main.tf
+++ modules/RDS/main.tf
@@ -1,12 +1,13 @@
-resource "aws_db_instance" "default" {
+resource "aws_db_instance" "my_database" {
   allocated_storage    = 10
-  db_name              = "postgresql"
-  engine               = "postgresql"
-  engine_version       = "5.7"
+  db_name              = "postgres"
+  engine               = "postgres"
+  engine_version       = "12.9"
   instance_class       = "db.t2.micro"
   username             = "postgres"
   password             = "foobarbaz"
-  parameter_group_name = "default.mysql5.7"
+  #Sparameter_group_name = aws_db_parameter_group.my_databse.name
+                          #aws_db_subnet_group.education.name
   skip_final_snapshot  = true
   vpc_security_group_ids = [var.sg]
   db_subnet_group_name  = aws_db_subnet_group.rds.name
